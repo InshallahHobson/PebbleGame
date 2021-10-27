808916d9c6164103c02420bfd1fca3052eda3b22
JasonJashari <73113041+JasonJashari@users.noreply.github.com>
2021-10-27 14:32:00 +0100

    player inner class, debugging

--- src/PebbleGame.java
+++ src/PebbleGame.java
@@ -13,6 +13,36 @@ public class PebbleGame {
     private ArrayList<Pebble> bagZ = new ArrayList<Pebble>();
     static Random randomNumGen = new Random();
 
+    public class Player {
+        private Pebble[] pebblesInHand = new Pebble[10];
+
+        public Player() {
+            chooseStartingPebbles();
+        }
+
+        public void chooseStartingPebbles() {
+            double randNum = randomNumGen.nextDouble();
+            if (randNum < (1.0/3.0)) {
+                for (int i = 0; i <= 9; i++) {
+                    pebblesInHand[i] = drawFrom(bagX);
+                }
+            }
+            if (randNum < (2.0/3.0)) {
+                for (int i = 0; i <= 9; i++) {
+                    pebblesInHand[i] = drawFrom(bagY);
+                }
+            } else {
+                for (int i = 0; i <= 9; i++) {
+                    pebblesInHand[i] = drawFrom(bagZ);
+                }
+            }
+        }
+
+        public Pebble[] getPebblesInHand(){
+            return pebblesInHand;
+        }
+    }
+
     public static Pebble drawFrom(ArrayList<Pebble> bag) {
         int index = randomNumGen.nextInt(bag.size());
         Pebble pebble = bag.get(index);
@@ -32,24 +62,6 @@ public class PebbleGame {
         }
     }
 
-    public void chooseStartingPebbles() {
-        double randNum = randomNumGen.nextDouble();
-        if (randNum < (1.0/3.0)) {
-            for (int i = 0; i <= 10; i++) {
-                Pebble pebble = drawFrom(bagX);
-            }
-        }
-        if (randNum < (2.0/3.0)) {
-            for (int i = 0; i <= 10; i++) {
-                drawFrom(bagY);
-            }
-        } else {
-            for (int i = 0; i <= 10; i++) {
-                drawFrom(bagZ);
-            }
-        }
-    }
-
     public static void initialiseBag(String bagLocation, ArrayList<Pebble> bag){
         try(BufferedReader br = new BufferedReader(new FileReader(bagLocation))){
             String line;
@@ -64,16 +76,11 @@ public class PebbleGame {
         }
     }
 
-    public static void main (String[] args)  {
-        new PebbleGame();
-    }
-
     public PebbleGame() {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the number of players:");
         this.noOfPlayers = input.nextInt();
         input.nextLine();
-        generateThreads();
 
         System.out.println("Please enter location of bag number 0 to load:");
         String bagLocation0 = input.nextLine();
@@ -86,6 +93,27 @@ public class PebbleGame {
         System.out.println("Please enter location of bag number 2 to load:");
         String bagLocation2 = input.nextLine();
         initialiseBag(bagLocation2, bagZ);
+    }
+
+    public void initialisePlayers(){
+        for (int i = 1; i <= this.noOfPlayers; i++) {
+            Player p = new Player();
+            Pebble[] ps = p.getPebblesInHand();
+
+
+
+            //new Thread(Integer.toString(i)) {
+                //public void run() {
+                    //chooseStartingPebbles();
+                    //drawPebble();
+                //}
+            //}.start();
+        }
+    }
+
+    public static void main (String[] args)  {
+        PebbleGame mainGame = new PebbleGame();
+        mainGame.initialisePlayers();
 
     }
 }
